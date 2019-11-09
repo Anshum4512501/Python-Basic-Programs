c38786911bf869acb93fa7ebeac222d3094e79f7
anshum45 <anshum45@gmial.com>
2019-11-09 22:58:22 +0530

    program to count number of a specific character

---
+++ countNumberOfCharacter.py
@@ -0,0 +1,12 @@
+s = input("Enter a string that you want to search in \n")
+def countChar(c):
+    count,j = 0,len(s)
+    for i in s :
+        j = j-1
+        if i==c:
+            count = count+1
+        if j == 0:
+             break
+    print(c , count)
+c = input("Enter a character that you need to search\n")
+countChar(c)
