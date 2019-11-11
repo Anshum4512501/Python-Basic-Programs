ab7a160d2c13742141b3d5e4f7cd5106e2c1b9a5
anshum45 <anshum45@gmial.com>
2019-11-11 23:20:45 +0530

    Some modification to file

--- Entering data into 2d  array dynamically.py
+++ Entering data into 2d  array dynamically.py
@@ -2,13 +2,18 @@ m = int(input("Enter number of rows \n"))
 n = int(input("Enter number of columns \n"))
 l = []
 arr =[]
-while m >=0:
-    while n>=0:
+# while m >=0:
+#     while n>=0:
+#         l.append(int(input("Enter a number \n")))
+#         n=n-1
+#         if(n==0):
+#             print("\n Enter another row")
+#     arr.append(l)
+#     m=m-1
+#
+
+for i in range(0,m-1):
+    for j in range(0,n-1):
         l.append(int(input("Enter a number \n")))
-        n=n-1
-        if(n==0):
-            print("\n Enter another row")
-    arr.append(l)    
-    m=m-1    
- 
+    arr.append(l[i])
 print(arr)
