34793e7a9be5f1a37a681be4c100b4c41b634613
anshum45 <anshum45@gmail.com>
2019-12-12 00:54:07 +0530

    Binary_Tree.py

---
+++ binarytree.py
@@ -0,0 +1,11 @@
+class BinaryTree:
+    def __init__(self,key):
+        self.left = None;
+        self.right = None;
+        self.value = key;
+    
+    
+node = BinaryTree(1)
+
+print(node.value)
+
