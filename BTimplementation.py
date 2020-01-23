377725bea764ad2bc10bb085d07f1be4854818dc
anshum45 <anshum45@gmail.com>
2020-01-23 20:27:41 +0530

    preorder postorder inorder

--- BTimplementation.py
+++ BTimplementation.py
@@ -7,3 +7,27 @@ class Node:
 tree=Node(1)
 tree.left=Node(2)
 tree.right=Node(3)
+
+def preOrder(tree):
+    if(tree):
+        print(tree.root)
+        preOrder(tree.left)
+        preOrder(tree.right)
+print(tree)
+preOrder(tree)
+def postOrder(tree):
+    if(tree):
+        postOrder(tree.left)
+        postOrder(tree.right)
+        print(tree.root)
+print(tree)
+postOrder(tree)
+def inOrder(tree):
+    if(tree):
+        
+        inOrder(tree.left)
+        print(tree.root)
+        inOrder(tree.right)
+        
+print(tree)
+inOrder(tree)
