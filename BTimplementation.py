152d480d7b2c346bc6306da0663e708424b4d7da
anshum45 <anshum45@gmail.com>
2020-01-23 20:48:09 +0530

    preorder postorder inorder completed

--- BTimplementation.py
+++ BTimplementation.py
@@ -3,14 +3,22 @@ class Node:
         self.root=key
         self.left=None
         self.right=None
-
+preorder=[]
+inorder=[]
+postorder=[]
 tree=Node(1)
 tree.left=Node(2)
+tree.right=Node(3)
+tree.left.left=Node(5)
+tree.left.right=Node(10)
+tree.left.left.left=Node(20)
+tree.left.left.right=Node(30)
+
 tree.right=Node(3)
 
 def preOrder(tree):
     if(tree):
-        print(tree.root)
+        preorder.append(tree.root)
         preOrder(tree.left)
         preOrder(tree.right)
 print(tree)
@@ -19,15 +27,18 @@ def postOrder(tree):
     if(tree):
         postOrder(tree.left)
         postOrder(tree.right)
-        print(tree.root)
+        postorder.append(tree.root)
 print(tree)
 postOrder(tree)
 def inOrder(tree):
     if(tree):
         
         inOrder(tree.left)
-        print(tree.root)
+        inorder.append(tree.root)
         inOrder(tree.right)
         
 print(tree)
 inOrder(tree)
+print("Inoder :",inorder)
+print("Preorder :",preorder)
+print("Postoder :",postorder)
